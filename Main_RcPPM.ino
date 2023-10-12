60f703e27eb9866de74eb29877554ce4df428703
A.G <59532613+Xarin94@users.noreply.github.com>
2023-10-12 16:04:34 +0200

    Update Main_RcPPM.ino
    made function to read/write mavlink

--- Main_RcPPM.ino
+++ Main_RcPPM.ino
@@ -1,6 +1,7 @@
 #include <RadioLib.h>
 #include <string.h>
 #include <PPMReader.h>
+#include <mavlink.h>
 
 //////////////pinout of the board//////////////
 //////////////////DO NOT CHANGE////////////////
@@ -19,15 +20,15 @@
 
 ///////////Radio params/////////////////////////
 
-#define RadioType 1       //1 air 0 gnd
+#define RadioType 0       //1 air 0 gnd
 #define Rxfreq 2425       //rx frequncy-> different from tx
 #define Txfreq 2475       //select between 2400 and 2480 mhz
 #define SPD 325           //speed kbps-> 325 / 650 / 1300
 #define TxPower -2        //output power -2 -> 34 dB, -18 ->20 dB  you can set any power in this range (check for legality)
 #define TelemID 1         //ID of he messages for the mavlink messages (0-254)
 #define RCbusID 12        //-1 if not used (additional id for sbus)
-#define RCbusrate 100     //delay in milliseconds for the sbus rate
-#define RCbuschannels 16  //numers of ppm channels
+#define RCbusrate 50     //delay in milliseconds for the sbus rate
+#define RCbuschannels 8  //numers of ppm channels
 #define RCbuspin 16       //Rx serial 4 pin
 ////////////////////////////////////////////////
 
@@ -44,7 +45,7 @@ uint8_t Message[MaxMessageSize];
 uint8_t MSG[MaxMessageSize];
 int msgrx = 0;
 int Rssi = 0;
-int recivedBytes = 0;
+int recivedBytes = 2;
 unsigned long int RCbusTimer = 0;
 unsigned long int msgtimeTX = 0;
 
@@ -92,17 +93,17 @@ void setFlagRX(void) {
 }
 
 void loop() {
-  if (Serial1.available() > 0 && recivedBytes < MaxMessageSize) {  //change Serial with Serial1 for the air, and vice versa
-    if (recivedBytes == 3) {
+  if (Serial.available() > 0 && recivedBytes < MaxMessageSize) {  //change Serial with Serial1 for the air, and vice versa
+    if (recivedBytes == 2) {
       Message[0] = TelemID;
       Message[1] = 0;
     }
-    Message[recivedBytes] = Serial1.read();  //change Serial with Serial1 for the air, and vice versa
+    Message[recivedBytes] = Serial.read();  //change Serial with Serial1 for the air, and vice versa
     recivedBytes++;
     msgtimeTX = millis();
   }
 
-  if (Serial1.available() == 0 && micros() - msgtimeTX > 500 && recivedBytes != 0) {  //change Serial with Serial1 for the air, and vice versa (this function sends incomplete messages after 50millis, for higherd bauds lower it)
+  if (Serial.available() == 0 && micros() - msgtimeTX > 500 && recivedBytes != 2) {  //change Serial with Serial1 for the air, and vice versa (this function sends incomplete messages after 50millis, for higherd bauds lower it)
     for (int i = recivedBytes; i < MaxMessageSize; i++) {
       Message[i] = 0;
     }
@@ -110,14 +111,17 @@ void loop() {
     msgtimeTX = millis();
   }
 
-  if (millis() - RCbusTimer >= RCbusrate && recivedBytes == 0) {
-    Message[0] = 255;  //specific for sbus messsage
+  if (millis() - RCbusTimer > RCbusrate && recivedBytes == 2 && RadioType == 0 && RCbusID !=-1) {
+    Message[0] = 254;  //specific for sbus messsage
     Message[1] = RCbusID;
-    for (int i = 0; i < RCbuschannels; i++) {
-      Message[i + 2] = ppm.latestValidChannelValue(i, 0);
+    for (int i = 0; i < RCbuschannels; i=i+2) {
+      Message[i + 2] = ppm.latestValidChannelValue(i, 0) & 0xff;
+      Message[i + 3] = (ppm.latestValidChannelValue(i, 0)>> 8);
     }
     RCbusTimer = millis();
-    recivedBytes=MaxMessageSize;
+    Serial.print(ppm.latestValidChannelValue(2,0));
+    Serial.print("  ,   ");
+    recivedBytes = MaxMessageSize;
   }
 
 
@@ -145,10 +149,28 @@ void loop() {
         Serial1.print(char(MSG[i]));  //mirror msg to ext port
       }
     }
-    if (MSG[0] == 255 && MSG[1] == RCbusID) {
+    if (MSG[0] == 254 && MSG[1] == RCbusID && RadioType == 1 && RCbusID !=-1) {
       //syntetize PPM out/ put in a mavlink message
+
+      mavlink_message_t message;
+
+      uint8_t target_system =255;
+      uint8_t target_component=254;
+      uint16_t ch1= (MSG[3] << 8) | MSG[2];
+      uint16_t ch2= (MSG[5] << 8) | MSG[4];
+      uint16_t ch3= (MSG[7] << 8) | MSG[6];
+      uint16_t ch4= (MSG[9] << 8) | MSG[8];
+      uint16_t ch5= (MSG[11] << 8) | MSG[10];
+      uint16_t ch6= (MSG[13] << 8) | MSG[12];
+      uint16_t ch7= (MSG[15] << 8) | MSG[14];
+      uint16_t ch8= (MSG[17] << 8) | MSG[16];
+
+      uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+       mavlink_msg_rc_channels_override_pack(255, 254, &message, target_system, target_component,ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8);
+      uint16_t len = mavlink_msg_to_send_buffer(buf, &message);
+      Serial1.write(buf, len);
     }
-    radio2.startReceive();  //starat reciving new msg
-    digitalWrite(LEDrx, LOW);
-  }
+  radio2.startReceive();  //starat reciving new msg
+  digitalWrite(LEDrx, LOW);
+}
 }
